8709dfad33b1a6dff4c6814aca8e8dbfe86d1b61
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-05-09 08:21:32 +0000

    [Finish]RspecTest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     @book = Book.new
@@ -13,15 +14,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+    is_matching_login_user
     @user = User.find(params[:id])
-    if @user == current_user
-        render "edit"
-    else
-      redirect_to user_path(current_user)
-    end
   end
 
   def update
+    is_matching_login_user
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice]="You have updated user successfully."
@@ -34,5 +32,12 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { minimum: 2, maximum: 20}
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20}
   validates :introduction, length: { maximum: 50 }
 
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
+  def get_profile_image(weight, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'dedault-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [weight, height]).processed
+  end
 end
